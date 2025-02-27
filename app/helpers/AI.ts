c0c766335a7db01de178456056827c1b98d638bd
NXB India <suport@nxbindia.com>
2025-02-27 10:40:18 +0530

    remove api key

--- app/helpers/AI.ts
+++ app/helpers/AI.ts
@@ -1,5 +1,3 @@
-const api_key ="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"
-
 import OpenAI from "openai";
 const openai = new OpenAI();
 export async function chm (){
